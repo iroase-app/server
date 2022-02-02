cd518d2388348426efa800d3306986c1aa004663
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-02-02 10:07:15 +0000

    create another test user that acts as someone else relative to our test user

--- src/__test__/setupAuth.ts
+++ src/__test__/setupAuth.ts
@@ -3,7 +3,16 @@ import * as db from '../db';
 /**
  * Cleans the DB and creates a test user for our tests to use.
  * The user has the username 'TestUser' and the token 'testToken'.
+ * This user has an id of 1.
  * The user is not a moderator.
+ *
+ * ---
+ *
+ * Another user is also created with the username 'someoneElse' and the id 999.
+ * They do not have an associated session.
+ *
+ * Use this user to mock things that belong to other accounts that the test user
+ * should not be able to access.
  * @returns {Promise<void>} A promise that resolves when everything is ready to go.
  */
 async function setupAuth(): Promise<void> {
@@ -21,6 +30,13 @@ async function setupAuth(): Promise<void> {
   VALUES ($1, $2, $3, $4);
   `,
   [user.rows[0].user_id, 'testToken', 'SAMSUNG Smart Toaster', new Date()]);
+
+  // I know this is scuffed.
+  await db.default.query(/* sql */ `
+  INSERT INTO "users" ("user_id", "username", "hashed_password", "created", "is_moderator")
+  OVERRIDING SYSTEM VALUE VALUES (999, 'someoneElse', 'this should be a hash', '2020-01-01', false);
+`);
+
   return Promise.resolve();
 }
 
