c93ddfa0844269a71020061e0a80ef3a29a29bc3
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-02-04 13:25:50 +0000

    reject bad requests

--- src/api/app/deck/patch.ts
+++ src/api/app/deck/patch.ts
@@ -4,15 +4,14 @@ import db from '../../../db';
 const updateDeck = express.Router();
 
 updateDeck.patch('/:id', async (req, res) => {
-  const deckId = req.params.id;
+  if (!req.body.name && !req.body.course && !req.body.public) return res.status(400).send({ error: 'noData' });
   const deck = await db.query(/* sql */ `
-  UPDATE "decks"
-  SET "decks"."name" = $1, "decks"."course" = $2, "decks"."public" = $3
-  JOIN "user_decks" ON "user_decks"."deck_id" = "decks"."deck_id"
-  WHERE "decks"."deck_id" = $4 AND "user_decks"."user_id" = $5;
-  `,
-  [req.body.name, req.body.course, req.body.public, deckId, req.user!.id]);
-  res.send(deck);
+    SELECT * FROM user_decks WHERE deck_id = $1;
+    `, [req.params.id]);
+  if (!deck.rows[0]) return res.status(404).send({ error: 'deckNotFound' });
+  if (deck.rows[0].user_id !== req.user!.id) return res.status(403).send({ error: 'notOwner' });
+  // TODO: Actually update the deck info.
+  return res.status(200).send({ error: deck.rows[0] });
 });
 
 export default updateDeck;
