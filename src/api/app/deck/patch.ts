22b60c9c1cf32b54eb2568530e1ef8b693df3c2b
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-02-22 12:43:10 +0000

    updating deck fields

--- src/api/app/deck/patch.ts
+++ src/api/app/deck/patch.ts
@@ -10,8 +10,34 @@ updateDeck.patch('/:id', async (req, res) => {
     `, [req.params.id]);
   if (!deck.rows[0]) return res.status(404).send({ error: 'deckNotFound' });
   if (deck.rows[0].user_id !== req.user!.id) return res.status(403).send({ error: 'notOwner' });
-  // TODO: Actually update the deck info.
-  return res.status(200).send({ error: deck.rows[0] });
+  const client = await db.connect();
+  await client.query('BEGIN');
+  try {
+    // Update the fields if they are provided.
+    if (req.body.name) {
+      await client.query(/* sql */ `
+        UPDATE decks SET name = $1 WHERE deck_id = $2;
+        `, [req.body.name, req.params.id]);
+    }
+    if (req.body.course) {
+      await client.query(/* sql */ `
+        UPDATE decks SET course = $1 WHERE deck_id = $2;
+        `, [req.body.course, req.params.id]);
+    }
+    if (req.body.public) {
+      await client.query(/* sql */ `
+        UPDATE decks SET public = $1 WHERE deck_id = $2;
+        `, [req.body.public, req.params.id]);
+    }
+    await client.query('COMMIT');
+    return res.send({ success: true });
+  } catch (e) {
+    await client.query('ROLLBACK');
+    console.error(`Error updating deck: ${e}`);
+    return res.status(500).send({ error: 'databaseError' });
+  } finally {
+    client.release();
+  }
 });
 
 export default updateDeck;
