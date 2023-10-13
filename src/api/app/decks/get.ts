53b80b8b8c7a4cba969bb6df84075436e0ff2215
Rain <emeraldsnorlax@disroot.org>
2023-10-13 14:18:53 +0100

    workaround for duplicate decks being returned

--- src/api/app/decks/get.ts
+++ src/api/app/decks/get.ts
@@ -6,10 +6,14 @@ const getDecks = express.Router();
 getDecks.get('/', async (req, res) => {
   const decks = await db.query(/* sql */ `
   SELECT "decks"."deck_id", "decks"."name", "decks"."course", "decks"."public" FROM "decks", "user_decks"
-  JOIN "users" ON "user_decks"."user_id" = "users"."user_id"
+  INNER JOIN "users" ON "user_decks"."user_id" = "users"."user_id"
   WHERE "user_decks"."user_id" = $1`,
   [req.user!.id]);
-  res.status(200).send({ decks: decks.rows });
+
+  res.status(200).send({
+    // TODO: Find a better way to remove duplicates as the query above returns duplicates
+    decks: [...new Map(decks.rows.map((deck) => [deck.deck_id, deck])).values()],
+  });
 });
 
 export default getDecks;
