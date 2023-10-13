c7ddd2ded26132bfcbfa36db7f60fe1fc0a9b9bb
Rain <emeraldsnorlax@disroot.org>
2023-10-13 16:09:40 +0100

    fix bug that sends over other people's decks

--- src/api/app/decks/get.ts
+++ src/api/app/decks/get.ts
@@ -5,15 +5,11 @@ const getDecks = express.Router();
 
 getDecks.get('/', async (req, res) => {
   const decks = await db.query(/* sql */ `
-  SELECT "decks"."deck_id", "decks"."name", "decks"."course", "decks"."public" FROM "decks", "user_decks"
-  INNER JOIN "users" ON "user_decks"."user_id" = "users"."user_id"
+  SELECT "decks"."deck_id", "decks"."name", "decks"."course", "decks"."public" FROM "decks"
+  INNER JOIN "user_decks" ON "decks"."deck_id" = "user_decks"."deck_id"
   WHERE "user_decks"."user_id" = $1`,
   [req.user!.id]);
-
-  res.status(200).send({
-    // TODO: Find a better way to remove duplicates as the query above returns duplicates
-    decks: [...new Map(decks.rows.map((deck) => [deck.deck_id, deck])).values()],
-  });
+  res.status(200).send({ decks: decks.rows });
 });
 
 export default getDecks;
